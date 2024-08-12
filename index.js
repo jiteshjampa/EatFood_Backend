b3249dcb240cdece5919b986c4769b76e9076e4f
J JITESH REDDY <jampajitesh@gmail.com>
2024-08-12 13:17:12 +0530

    updated the changes

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const mongodb = require('./db');
-const cors = require('cors');
-const express = require('express');
+const mongodb = require("./db");
+const cors = require("cors");
+const express = require("express");
 const app = express();
 const port = 5000;
-mongodb().then(async () => {
-    await app.use(cors(
-        {
-            origin: '*'
-        }
-
-    ));
+mongodb()
+  .then(async () => {
+    await app.use(
+      cors({
+        origin: "*",
+      })
+    );
     await app.use(express.json());
-    await app.get('/', (req, res) => {
-        res.send('Hello World!');
+    await app.get("/", (req, res) => {
+      res.send("Hello World!");
     });
-    await app.use('/api',require('./Routes/CreateUser'));
-    await app.use('/api',require('./Routes/Displaydata'));
-    await app.use('/api',require('./Routes/Orderdata'));
+    await app.use("/api", require("./Routes/CreateUser"));
+    await app.use("/api", require("./Routes/Displaydata"));
+    await app.use("/api", require("./Routes/Orderdata"));
     await app.listen(port, () => {
-        console.log(`food app listening on port ${port}`);
+      console.log(`food app listening on port ${port}`);
     });
-
-}).catch((err)=>{console.log(err)});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
